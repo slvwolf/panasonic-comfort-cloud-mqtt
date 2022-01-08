c39b3fdc2c541f0f0fd4d41980b50f930cecca03
SlvWolf <slvwolf@finfur.net>
2022-01-08 09:02:07 +0200

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='panasonic-comfort-cloud-mqtt',
-    version='0.1.0',
+    version='0.2.0',
     description='Home-Assistant MQTT bridge for Panasonic Comfort Cloud ',
     long_description=long_description,
     long_description_content_type="text/markdown",
