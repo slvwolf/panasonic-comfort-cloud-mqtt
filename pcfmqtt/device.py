9d39c8d196ba113f8afec874a073580607d498b4
SlvWolf <slvwolf@finfur.net>
2022-05-21 18:30:57 +0300

    Improved logging on state changes

--- pcfmqtt/device.py
+++ pcfmqtt/device.py
@@ -1,3 +1,4 @@
+import re
 from time import time
 import paho.mqtt.client as mqtt
 import pcomfortcloud
@@ -5,9 +6,15 @@ from pcfmqtt.events import state_event
 import pcfmqtt.mappings as mappings
 from pcomfortcloud import constants
 
+def log_if_updated(current_value: any, new_value: any, device_name: str, value_name: str):
+    if current_value != new_value:
+        print("%s: Updated %s ( %r -> %r )" % (device_name, value_name, current_value, new_value))
+    return new_value
+
 class DeviceState:
 
-    def __init__(self, params: dict) -> None:
+    def __init__(self, name: str, params: dict) -> None:
+        self.name = name
         self.temperature = params.get("temperature", 0) # type: float
         self.power = params.get("power", constants.Power.Off) # type: constants.Power
         self.temperature_inside = params.get("temperatureInside", 0) # type: int
@@ -19,19 +26,27 @@ class DeviceState:
         self.eco = params.get("eco", constants.EcoMode.Auto) # type: constants.EcoMode
         self.nanoe = params.get("nanoe", constants.NanoeMode.On) # type: constants.NanoeMode
 
+    def refresh_all(self, state: "DeviceState"):
+        """
+        Refresh the full state, including read-only metrics
+        """
+        self.refresh(state)
+        self.temperature_inside = log_if_updated(self.temperature_inside, state.temperature_inside, self.name, "temperature inside")
+        self.temperature_outside = log_if_updated(self.temperature_outside, state.temperature_outside, self.name, "temperature outside")
+    
     def refresh(self, state: "DeviceState"):
         """
         Refresh the state from the given one. This is to update the current state from desired when device has been succesfully
         update. 
         """
-        self.temperature = state.temperature
-        self.power = state.power
-        self.mode = state.mode
-        self.fan_speed = state.fan_speed
-        self.air_swing_horizontal = state.air_swing_horizontal
-        self.air_swing_vertical = state.air_swing_vertical
-        self.eco = state.eco
-        self.nanoe = state.nanoe
+        self.temperature = log_if_updated(self.temperature, state.temperature, self.name, "temperature")
+        self.power = log_if_updated(self.power, state.power, self.name, "power")
+        self.mode = log_if_updated(self.mode, state.mode, self.name, "mode")
+        self.fan_speed = log_if_updated(self.fan_speed, state.fan_speed, self.name, "fan speed")
+        self.air_swing_horizontal = log_if_updated(self.air_swing_horizontal, state.air_swing_horizontal, self.name, "air swing horizontal")
+        self.air_swing_vertical = log_if_updated(self.air_swing_vertical, state.air_swing_vertical, self.name, "air swing vertical")
+        self.eco = log_if_updated(self.eco, state.eco, self.name, "eco")
+        self.nanoe = log_if_updated(self.nanoe, state.nanoe, self.name, "nanoe")
 
 class Device:
 
@@ -66,14 +81,13 @@ class Device:
             }
         """
         if self._target_refresh < time():
-            print("Retrieving data for {}/{} ({})".format(self._name, self._ha_name, self._id))
+            print("{}: Retrieving data".format(self.get_name()))
             data = session.get_device(self._id)
-            self._state = DeviceState(data["parameters"])
+            self._state.refresh_all(DeviceState(self.get_name(), data["parameters"]))
             # If we have not initialized the device yet, assume current params are the desired state
             if not self._desired_state:
-                self._desired_state = DeviceState(data["parameters"])
+                self._desired_state = DeviceState(self.get_name(), data["parameters"])
             self._target_refresh = time() + refresh_delay
-            print("Data received for {}/{} ({})".format(self._name, self._ha_name, self._id))
             return True
         return False
 
